50a6be7aa1fb924d6049a185788376a0f6f2bfa6
Biswarup35 <biswarup35@gmail.com>
2022-04-14 13:21:26 +0530

    adding global css

--- packages/material-meeu/src/theme/ThemeProvider.tsx
+++ packages/material-meeu/src/theme/ThemeProvider.tsx
@@ -1,5 +1,10 @@
-import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
-import { ReactNode, useEffect } from "react";
+import {
+  ThemeProvider as EmotionThemeProvider,
+  Global,
+  css,
+} from "@emotion/react";
+import { useMemo } from "react";
+import { ReactNode } from "react";
 import { useSnapshot } from "valtio";
 import d_theme, { state } from "./theme";
 
@@ -10,27 +15,48 @@ interface IThemeProvider {
 }
 
 const ThemeProvider: React.FC<IThemeProvider> = (props) => {
-  const { theme, ...rest } = props;
+  const { theme, children, ...rest } = props;
   const { type } = useSnapshot(state);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      let body = document.querySelector("body");
-      if (body) {
-        if (type === "dark") {
-          body.style.backgroundColor = `rgba(0, 0, 0, 1)`;
-          body.style.color = `rgba(232, 222, 248, 1)`;
-        } else {
-          body.style.backgroundColor = `rgba(255, 255, 255, 1)`;
-          body.style.color = `rgba(black, 1)`;
-        }
-      }
-    }
-  }, [type]);
+  const _bg = useMemo(
+    () =>
+      new Map([
+        ["light", "rgba(255, 255, 255, 1)"],
+        ["dark", "rgba(0, 0, 0, 1)"],
+      ]),
+    [type]
+  ).get(type);
+  const _color = useMemo(
+    () =>
+      new Map([
+        ["light", "rgba(0, 0, 0, 1)"],
+        ["dark", "rgba(232, 222, 248, 1)"],
+      ]),
+    [type]
+  ).get(type);
 
   // Override the default theme with the one passed in
   const app_theme = Object.assign(d_theme, theme);
-  return <EmotionThemeProvider theme={app_theme} {...rest} />;
+  return (
+    <EmotionThemeProvider theme={app_theme} {...rest}>
+      <Global
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+          }
+          body {
+            background: ${_bg};
+            color: ${_color};
+          }
+        `}
+      />
+      {children}
+    </EmotionThemeProvider>
+  );
 };
 
 export default ThemeProvider;
